20f02d84c48a7e8a4b8ab28a386b349d0f9a029c
Kang-Min-Ku <netisen4@gmail.com>
2023-07-20 23:30:11 +0900

    change directory pattern

--- experiments/distributed/recommandation/config/generate_configs.py
+++ experiments/distributed/recommandation/config/generate_configs.py
@@ -20,24 +20,24 @@ with open("base.yaml") as fd:
 #-------------------------------------correct below--------------------------------------------
 #config variation set
 #should set same key of config.yaml
-var_keys = ["test_type",
-            "dataset",
-            "send_telegram_after_complete",
-            "seed",
-            "bpr_reg",
-            "ssl_reg",
-            "proto_reg"
-            ]
-case_test_type = ["LossContribution"]
+var_keys = [
+    "test_type",
+    "dataset",
+    "send_telegram_after_complete",
+    "seed",
+    "sl_use_user_gdve_plus",
+    "sl_use_item_gdve_plus",
+    "use_CL"
+]
+case_test_type = ["Test"]
 case_dataset = ["ml-100k"] #, "gowalla", "yelp2018"
 case_telegram = [True]
-case_seed = rd.sample(range(10000), 3) #also act as repeatition of test
-case_bpr = [0.1, 0.01, 0.001, 0.0001]
-case_ssl = [1e-5, 1e-6, 1e-7, 1e-8]
-case_proto = [8e-5, 8e-6, 8e-7, 8e-8]
+case_seed = rd.sample(range(10000), 1) #also act as repeatition of test
+case_gdve_plus = [True, False]
+case_CL = [False]
 
 #should match order of config key
-var_set = [case_test_type, case_dataset, case_telegram, case_seed, case_bpr, case_ssl, case_proto]
+var_set = [case_test_type, case_dataset, case_telegram, case_seed, case_gdve_plus, case_gdve_plus, case_CL]
 #----------------------------------------------------------------------------------------------
 assert len(var_keys) == len(var_set)
 
@@ -52,14 +52,17 @@ for var_values in itertools.product(*var_set):
         base_config[key] = val
         # if key == "dataset":
         #     base_config["data_path"] = f"./../../../data/{val}"
+    
+    if base_config["sl_use_user_gdve_plus"] != base_config["sl_use_item_gdve_plus"]:
+        continue
 
     # if var_values[-1] != var_values[-2]:
     #     continue
-    if not os.path.exists(f"{args.path}{file_index}"):
-        os.makedirs(f"{args.path}{file_index}")
+    if not os.path.exists(f"{args.path}_{file_index}"):
+        os.makedirs(f"{args.path}_{file_index}")
     
-    with open(os.path.join(f"{args.path}{file_index}", f"{base_config['test_type']}_{base_config['dataset']}_{token}.yaml"), "w") as fd:
+    with open(os.path.join(f"{args.path}_{file_index}", f"{base_config['test_type']}_{base_config['dataset']}_{token}.yaml"), "w") as fd:
         yaml.dump(base_config, fd)
 
-    if len(os.listdir(f"{args.path}{file_index}")) > num_file_in_dir:
+    if len(os.listdir(f"{args.path}_{file_index}")) > num_file_in_dir:
         file_index += 1
